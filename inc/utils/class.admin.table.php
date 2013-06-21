13052b828ed49e2508dc29f1dbe463fd5782af1e
Nicolas Juen <njuen@beapi.fr>
2013-06-21 10:35:59 +0200

    Format, order by desc by default

--- inc/utils/class.admin.table.php
+++ inc/utils/class.admin.table.php
@@ -129,7 +129,7 @@ class Bea_Sender_Admin_Table extends WP_List_Table {
 		// If no sort, default to title
 		$_orderby = (!empty( $_GET[ 'orderby' ] ) && in_array( $_GET[ 'orderby' ], self::$auth_order )) ? $_GET[ 'orderby' ] : 'id';
 		// If no order, default to asc
-		$_order = (!empty( $_GET[ 'order' ] ) && in_array( $_GET[ "order" ], array( 'asc', 'desc' ) )) ? $_GET[ "order" ] : 'asc';
+		$_order = (!empty( $_GET[ 'order' ] ) && in_array( $_GET[ "order" ], array( 'asc', 'desc' ) )) ? $_GET[ "order" ] : 'desc';
 		$order_by = " ORDER BY $_orderby $_order";
 
 		// Make the order
@@ -145,7 +145,7 @@ class Bea_Sender_Admin_Table extends WP_List_Table {
 			c.add_date, 
 			c.scheduled_from, 
 			c.from, 
-			c.from_name, 
+			c.from_name,
 			c.subject
 		FROM 
 		$wpdb->bea_s_campaigns as c
