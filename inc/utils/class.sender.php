6adc3352f2aa623dbb7e5817a84e635400f182b3
Nicolas Juen <njuen@beapi.fr>
2013-07-09 10:27:32 +0200

    Formating

--- inc/utils/class.sender.php
+++ inc/utils/class.sender.php
@@ -1,42 +1,44 @@
 <?php
 class Bea_Sender_Sender {
-	
-	private $campaigns = array();
-	
-	function __construct() {}
-	
-	public function init() {
-		if( !$this->getCampaigns() ) {
-			return false;
-		}
-		
-		return $this->sendCampaigns();
+
+	private $campaigns = array( );
+
+	function __construct( ) {
 	}
-	
-	private function getCampaigns() {
-		global $wpdb;
-		
-		$cols = $wpdb->get_col( "SELECT id FROM $wpdb->bea_s_campaigns WHERE current_status IN( '".implode( "','", Bea_Sender_Campaign::getAuthStatuses() )."' ) AND scheduled_from <= '".current_time( 'mysql' )."' ORDER BY add_date ASC" );
-		
-		if( !isset( $cols ) || empty( $cols ) ) {
-			$this->campaigns = array();
+
+	public function init( ) {
+		if( !$this->getCampaigns( ) ) {
 			return false;
 		}
-		$this->campaigns = array_map( 'absint' ,$cols );
+
+		return $this->sendCampaigns( );
+	}
+
+	private function getCampaigns( ) {
+		global $wpdb;
+
+		$cols = $wpdb->get_col( "SELECT id FROM $wpdb->bea_s_campaigns WHERE current_status IN( '".implode( "','", Bea_Sender_Campaign::getAuthStatuses( ) )."' ) AND scheduled_from <= '".current_time( 'mysql' )."' ORDER BY add_date ASC" );
+
+		if( !isset( $cols ) || empty( $cols ) ) {
+			$this->campaigns = array( );
+			return false;
+		}
+		$this->campaigns = array_map( 'absint', $cols );
 		return true;
 	}
-	
-	private function sendCampaigns() {
-		$results = array();
+
+	private function sendCampaigns( ) {
+		$results = array( );
 		foreach( $this->campaigns as $campaign_id ) {
 			$campaign = new Bea_Sender_Campaign( $campaign_id );
-			if( $campaign->isData() !== true ) {
+			if( $campaign->isData( ) !== true ) {
 				continue;
 			}
-			
+
 			// Make the sending
-			$results[] = $campaign->makeSend();
+			$results[] = $campaign->makeSend( );
 		}
 		return $results;
 	}
+
 }
