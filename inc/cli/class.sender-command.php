e376e2be94d0b6a7a7cef18cc6c50927851e5df6
Léonard Phoumpakka <lphoumpakka@beapi.fr>
2021-08-24 16:24:38 +0200

    PR NJ

--- inc/cli/class.sender-command.php
+++ inc/cli/class.sender-command.php
@@ -36,64 +36,10 @@ class Bea_Sender_Command extends \WP_CLI_Command {
 		$sender = $sender->init();
 
 		if ( empty( $sender ) ) {
-			\WP_CLI::error( 'The mailing campaigns could not be sent' );
+			\WP_CLI::info( sprintf( 'No campaign to send. You can see more with log : %s', WP_CONTENT_DIR . '/bea-sender-email-cron' ) );
+			return;
 		}
 
 		\WP_CLI::success( 'The mailing campaigns were sent' );
-	}
-
-	/**
-	 * Bea sender to prepare test process
-	 *
-	 * @param $args
-	 * @param $assoc_args
-	 *
-	 * @throws \WP_CLI\ExitException
-	 *
-	 * @author Léonard Phoumpakka
-	 *
-	 * ## EXAMPLES
-	 *
-	 * wp bea-sender-mail prepare-test-mail
-	 *
-	 * @subcommand prepare-test-mail
-	 *
-	 */
-	public function prepare_send_test( $args, $assoc_args ) {
-
-		$emails = isset( $assoc_args['emails'] ) ? explode( ',', $assoc_args['emails'] ) : [];
-
-		$datas = [];
-
-		if ( ! empty( $emails ) ) {
-			foreach ( $emails as $email ) {
-				if ( ! is_email( $email ) ) {
-					continue;
-				}
-				$datas[] = [
-					'email' => sanitize_email( $email ),
-					'html'  => '<p>Test email for ' . $email . '</p>',
-				];
-			}
-		}
-
-		if ( empty( $datas ) ) {
-			\WP_CLI::error( 'No recipients found' );
-		}
-
-		$data_campaign = [
-			'from'      => 'test@bea-sender.fr',
-			'from_name' => 'BEA Sender',
-			'subject'   => 'BEA Sender - ' . date_i18n( 'd/m/Y', time() ),
-		];
-
-		$campaign = new \Bea_Sender_Campaign();
-		$insert   = $campaign->add( $data_campaign, $datas );
-
-		if ( ! $insert ) {
-			\WP_CLI::error( 'Problem to prepared the mailing campaigns' );
-		}
-
-		\WP_CLI::success( 'The mailing campaigns were prepared' );
 	}
 }
